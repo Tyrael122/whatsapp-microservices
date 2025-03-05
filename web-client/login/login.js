ef9dad0b736dced37dbd9a7c0baf3c1aa02d4633
tyrael <kanbme@protonmail.com>
2025-03-05 10:46:03 -0300

    Deleting unused files

--- web-client/login/login.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    if (username) {
-        window.location.href = `../chat/chat.html?username=${encodeURIComponent(username)}`;
-    }
-});
